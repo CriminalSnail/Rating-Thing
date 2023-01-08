b4cb0eade45e1925e0302e807e78caa04a7c67b5
CriminalSnail <110540642+CriminalSnail@users.noreply.github.com>
2023-01-08 15:34:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+var arr=[];
+    function selected (number) {
+        arr.push(number);}
+    
+       function apply(app) {if (arr[0]>0&&app==0)
+    {document.getElementById("selected").innerHTML=`You selected ${arr[0]} out of 5`;
+    document.getElementById("container").style.display="none";
+    document.getElementById("container2").style.display="block"}
+    else {alert("Please, rate us")}}
